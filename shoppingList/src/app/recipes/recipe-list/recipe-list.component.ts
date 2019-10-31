98c010ab6e83a6fb262ff7812c8dd05137d46f15
Alvin Lee <404error@Alvinde-MBP.hitronhub.home>
2019-10-31 01:40:00 -0700

    In progress

--- shoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,19 +7,26 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe',
-      'this is a test for description',
+      'A test Recipe Tomato',
+      'this is a test for Tomato',
       'https://assets.epicurious.com/photos/5d49f1ea888da200097dce24/6:4/w_620%2Ch_413/TomatoesHerbs_RECIPE_073119_278.jpg'
     ),
     new Recipe(
-      'A test Recipe',
-      'this is a test for description',
+      'A test Recipe Potato',
+      'this is a test for Potato',
       'https://assets.epicurious.com/photos/5d49f1ea888da200097dce24/6:4/w_620%2Ch_413/TomatoesHerbs_RECIPE_073119_278.jpg'
     )
   ];
   constructor() {}
 
   ngOnInit() {}
+
+onSelectedRecipe(recipe: Recipe) {
+  this.recipeWasSelected.emit(recipe);
+}
+
 }
